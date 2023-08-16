432a72bcd5342113df759f6a1c9925b0beb1c3c9
thiagombdev <eu.thiagob3@gmail.com>
2023-08-16 09:19:46 -0300

    k

---
+++ src/models/cursos.ts
@@ -0,0 +1,19 @@
+import { Entity, PrimaryColumn, Column } from "typeorm"
+@Entity("curso")
+export default class curso {
+  @PrimaryColumn()
+  id_curso:string
+  @Column()
+  descricao_curso:string
+  @Column()
+  carga_horaria_curso:number
+  @Column()
+  modalidade:string
+  @Column()
+  eixo:string
+  @Column()
+  id_turma:string
+  @Column()
+  id_aula:string
+}
+
