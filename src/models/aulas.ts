ed79b952ac5c4670c2794bb9fe8e0a828a3b0288
jonasjuniordev <142232914+jonasjuniordev@users.noreply.github.com>
2023-08-16 09:30:06 -0300

    model

---
+++ src/models/aulas.ts
@@ -0,0 +1,17 @@
+import {Entity, PrimaryColumn, Column} from "typeorm"
+
+@Entity("aula")
+export default class Aula{
+
+@PrimaryColumn()
+id_aula: String
+@Column({nullable: false})
+data_aula: Date
+@Column({nullable: false})
+status_aula: String
+@Column()
+fk_tuma: String
+@Column()
+fk_unidade: String
+
+}
