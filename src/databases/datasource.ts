0a33d661500d7ebe4fcf7bbefeba33f65551acaf
brunaalves <bruhousalves@gmail.com>
2023-08-16 09:30:36 -0300

    model

--- src/databases/datasource.ts
+++ src/databases/datasource.ts
@@ -10,17 +10,3 @@ database: "cronogramas",
 synchronize: true,
 logging: true,
 })
-
-import { DataSource } from "typeorm"
-
-export const AppDataSource = new DataSource({
-  type: "postgres",
-  host: "localhost",
-  port: 5432,
-  username: "postgres",
-  password: "postgres",
-  database: "cronogramas",
-  synchronize: true,
-  logging: true,
-})
-
