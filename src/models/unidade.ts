33e81b412b73f5721fc4d6ed5bff1131ca2217f7
joaopedromarque <j.p.marques.a@gmail.com>
2023-08-16 09:11:17 -0300

    Create unidade.ts

---
+++ src/models/unidade.ts
@@ -0,0 +1,18 @@
+
+
+import { Entity, PrimaryColumn, Column} from "typeorm"
+@Entity("unidade")
+export default class Unidade {
+  //primary key
+  @PrimaryColumn()
+  id_unidade:string
+  //atributos
+  @Column()
+  descricao_unidade:string
+  @Column()
+  carga_horaria_unidade:number 
+  @Column()
+  ordem:number 
+  @Column()
+  fk_curso:string 
+}
