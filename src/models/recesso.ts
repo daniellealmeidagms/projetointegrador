8d22610d5396a0151f66925ef86e3b3a49fd8410
Davi Augusto Bitencourt <davibitencourt@yahoo.com>
2023-08-16 09:30:04 -0300

    model

---
+++ src/models/recesso.ts
@@ -0,0 +1,18 @@
+import {Entity, PrimaryColumn, Column} from "typeorm"
+
+@Entity("recesso")
+export default class Recesso {
+
+  @PrimaryColumn()
+  id_recesso: string
+
+  @Column({nullable: false})
+  descricao_recesso: string
+
+  @Column({nullable: false})
+  data_recesso: Date
+
+    constructor(){
+      
+    }
+}
