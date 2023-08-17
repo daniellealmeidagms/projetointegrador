d01ddd8f25fd81173be938516dc0a3535a3ff0bf
rgbds <rgbds293@gmail.com>
2023-08-17 10:59:17 -0300

    services e bd

---
+++ src/models/cursos.ts
@@ -0,0 +1,15 @@
+import { Entity, PrimaryColumn, Column } from "typeorm"
+@Entity("curso")
+export default class curso {
+  @PrimaryColumn()
+  id_curso: string
+  @Column({ nullable: false })
+  descricao_curso: string
+  @Column({ nullable: false })
+  carga_horaria_curso: number
+  @Column({ nullable: false })
+  modalidade: string
+  @Column()
+  eixo: string
+}
+ 
---
+++ src/models/servicesCursos.ts
@@ -0,0 +1,59 @@
+import { AppDataSource } from "../databases/datasource"
+import Curso from "../models/cursos"
+const cursor = AppDataSource.getRepository(Curso)
+export class servicesCursos {
+  async create(
+    descricao_curso,
+    carga_horaria_curso,
+    modalidade,
+    eixo
+  ): Promise<Curso | Error> {
+    if (await cursor.findOne({ where: { descricao_curso } })) {
+      return new Error("Curso já registrado!")
+    }
+    const curso = cursor.create({
+      carga_horaria_curso,
+      modalidade,
+      eixo,
+    })
+    await cursor.save(curso)
+    return curso
+  }
+  async ReadAll() {
+    const cursos = await cursor.find()
+    return cursos
+  }
+  async readonly(id_curso) {
+    const curso = await cursor.findOne({ where: { id_curso } })
+    if (!curso) {
+      return new Error("Curso não existente!")
+    }
+    return curso
+  }
+  async update(
+    id_curso,
+    carga_horaria_curso,
+    modalidade,
+    eixo
+  ): Promise<Curso | Error> {
+    const curso = await cursor.findOne({ where: { id_curso } })
+    if (!curso) {
+      return new Error("Curso não existente!")
+    }
+    curso.carga_horaria_curso = carga_horaria_curso
+      ? carga_horaria_curso
+      : curso.carga_horaria_curso
+    curso.modalidade = modalidade ? modalidade : curso.modalidade
+    curso.eixo = eixo ? eixo : curso.eixo
+    await cursor.save(curso)
+    return curso
+  }
+  async delete(id_curso) {
+    const curso = await cursor.findOne({ where: { id_curso } })
+    if (!curso) {
+      return new Error("Curso não existente!")
+    }
+    await cursor.delete(curso.id_curso)
+    return "Curso excluido com sucesse!"
+  }
+}
