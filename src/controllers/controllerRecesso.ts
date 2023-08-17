ed85b46063e939984b2733e92eddd5bb001324b4
Joengleson Pereira <juengsfinance@gmail.com>
2023-08-17 11:46:58 -0300

    socorro deus

---
+++ src/controllers/controllerRecesso.ts
@@ -0,0 +1,11 @@
+import { Request, Response } from "express"
+
+export class ControllerRecesso {
+  async create(request: Request, response: Response) {}
+  async readAll(request: Request, response: Response) {
+    
+  }
+  async readone(request: Request, response: Response) {}
+  async update(request: Request, response: Response) {}
+  async delete(request: Request, response: Response) {}
+}
--- src/models/recesso.ts
+++ src/models/recesso.ts
@@ -1,17 +1,18 @@
 import { Entity, PrimaryColumn, Column } from "typeorm"
+import { v4 as uuid } from "uuid"
 
 @Entity("recesso")
 export default class Recesso {
   @PrimaryColumn()
-    id_recesso: string;
+  id_recesso: string
 
   @Column({ nullable: false })
-  descricao_recesso: string;
+  descricao_recesso: string
 
   @Column({ nullable: false })
-  data_recesso: Date;
+  data_recesso: Date
 
-  constructor () {
-    
+  constructor() {
+    this.id_recesso = uuid()
   }
 }
