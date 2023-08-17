b5256dd8e2d3b5acc5f0f4f70bb5115c9af13f99
joaopedromarque <j.p.marques.a@gmail.com>
2023-08-17 11:43:46 -0300

    s

--- src/models/unidade.ts
+++ src/models/unidade.ts
@@ -1,21 +1,18 @@
-
-
-import { Entity, PrimaryColumn, Column} from "typeorm"
+import { Entity, PrimaryColumn, Column } from "typeorm"
+import { v4 as uuid } from "uuid"
 @Entity("unidade")
 export default class Unidade {
-  //primary key
   @PrimaryColumn()
-  id_unidade:string
-  //atributos
-  @Column({nullable: false})
-  descricao_unidade:string
-  @Column({nullable: false})
-  carga_horaria_unidade:number 
-  @Column({nullable: false})
-  ordem:number 
+  id_unidade: string
+  @Column({ nullable: false })
+  descricao_unidade: string
+  @Column({ nullable: false })
+  carga_horaria_unidade: number
+  @Column({ nullable: false })
+  ordem: number
   @Column()
-  fk_curso:string 
-  constructor(){
-
+  fk_curso: string
+  constructor() {
+    this.id_unidade = uuid()
   }
 }
