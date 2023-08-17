83f691393dc4daaa11f798fbf677c593d3000551
luisgmfdev <luisbielzinho10012002@hotmail.com>
2023-08-17 10:59:17 -0300

    services e bd

--- src/models/aula.ts
+++ src/models/aula.ts
@@ -1,18 +1,15 @@
-import { Entity,PrimaryColumn,Column } from 'typeorm';
-@Entity('aula')
-
-export default class aula{
+import { Entity, PrimaryColumn, Column } from "typeorm"
+@Entity("aula")
+export default class Aula {
   @PrimaryColumn()
-  id_aulas:string
-@Column({nullable:false})
-data_aula:Date
-@Column({nullable:false})
-status_aula:string
-@Column()
-fk_turma:string
-@Column()
-fk_unidade:string
-constructor(){
-
-}
+  id_aulas: string
+  @Column({ nullable: false })
+  data_aula: Date
+  @Column({ nullable: false })
+  status_aula: string
+  @Column()
+  fk_turma: string
+  @Column()
+  fk_unidade: string
+  constructor() {}
 }
---
+++ src/services/serviceAula.ts
@@ -0,0 +1,51 @@
+import { AppDataSource } from "../databases/datasource"
+import Aula from "../models/aula"
+const cursor = AppDataSource.getRepository(Aula)
+export class Serviceaula {
+  async create(data_aula, status_aula,fk_turma,fk_unidade): Promise<Aula | Error> {
+    if (await cursor.findOne({ where: { data_aula } })) {
+      return new Error("Aula já cadastrada!")
+    }
+
+    const aula = cursor.create({
+      data_aula,
+      status_aula,
+      fk_turma,
+      fk_unidade
+    })
+    await cursor.save(aula)
+    return aula
+  }
+
+  async readAll() {
+    const aulas = await cursor.find()
+    return aulas
+  }
+  async readOne(id_aulas) {
+    const aula = await cursor.findOne({ where: { id_aulas } })
+    if (!aula) {
+      return new Error("Aula não encontrada!")
+    }
+    return aula
+  }
+  async update(id_aulas, data_aula, status_aula,fk_turma,fk_unidade): Promise<Aula | Error> {
+    const aula = await cursor.findOne({ where: { id_aulas } })
+    if (!aula) {
+      return new Error("Aula não encontrada!")
+    }
+    aula.data_aula = data_aula ? data_aula : aula.data_aula
+    aula.status_aula = status_aula ? status_aula : aula.data_aula
+    aula.fk_turma= fk_turma? fk_turma:aula.fk_turma
+    aula.fk_unidade=fk_unidade? fk_unidade:fk_unidade
+    await cursor.save(aula)
+    return aula
+  }
+  async delete(id_aulas) {
+    const aula = await cursor.findOne({ where: { id_aulas } })
+    if (!aula) {
+      return new Error("Aula não encontrada! ")
+    }
+    await cursor.delete(aula.id_aulas)
+    return "Aula excuída com sucesso!"
+  }
+}
