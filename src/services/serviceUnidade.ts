03964c6fa604fd02a43b094c1dc55b288cbe818d
Patrick Pablo <138041918+pilgrimpablo@users.noreply.github.com>
2023-08-17 10:59:29 -0300

    services e bd

--- src/services/serviceUnidade.ts
+++ src/services/serviceUnidade.ts
@@ -4,35 +4,53 @@ import Unidade from "../models/unidade"
 const cursor = AppDataSource.getRepository(Unidade)
 
 export class ServiceRecesso {
-  async create(descricao_unidade,carga_horaria_unidade,ordem): Promise<Unidade| Error>{
-    if (await cursor.findOne({ where: {descricao_unidade,carga_horaria_unidade,ordem}})){
-      return new Error ("Unidade ja cadastrada")
+  async create(descricao_unidade, carga_horaria_unidade, ordem): Promise<Unidade | Error> {
+    if (await cursor.findOne({ where: { descricao_unidade } })) {
+      return new Error("Unidade ja cadastrada")
     }
     const unidade = cursor.create({
       descricao_unidade,
       carga_horaria_unidade,
-      ordem
+      ordem,
     })
+    await cursor.save(unidade)
+    return unidade
   }
-  async readAll(){
+  async readAll() {
     const unidades = await cursor.find()
     return unidades
   }
-  async readOne( id_unidade){
-    const unidade = await cursor.findOne({ where:{ id_unidade }})
-    if(!unidade){
+  async readOne(id_unidade) {
+    const unidade = await cursor.findOne({ where: { id_unidade } })
+    if (!unidade) {
       return new Error("Unidade nao encontrada")
     }
     return unidade
   }
-  async update(){}
-  async delete(id_unidade){
-    const unidade = await cursor.findOne({ where:{ id_unidade }})
-    if(!unidade){
-      return new Error("Unidade nao encontrada")
+  async update(
+    id_unidade,
+    descricao_unidade,
+    carga_horaria_unidade,
+    ordem
+  ): Promise<Unidade | Error> {
+    const unidade = await cursor.findOne({ where: { id_unidade } })
+    if (!unidade) {
+      return new Error("Unidade nao encontrada!")
+    }
+    unidade.descricao_unidade = descricao_unidade ? descricao_unidade : unidade.descricao_unidade
+    unidade.carga_horaria_unidade = carga_horaria_unidade
+      ? carga_horaria_unidade
+      : unidade.carga_horaria_unidade
+    unidade.ordem = ordem ? ordem : unidade.ordem
+    await cursor.save(unidade)
+    return unidade
   }
-  await cursor.delete(unidade.id_unidade)
-  return "Unidade excluida com sucesso"
-}
-
+  async delete(id_unidade) {
+    const unidade = await cursor.findOne({ where: { id_unidade } })
+    if (!unidade) {
+      return new Error("Unidade nao encontrada")
+    }
+    await cursor.delete(unidade.id_unidade)
+    return "Unidade excluida com sucesso"
+  }
 }
