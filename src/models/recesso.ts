94861c86d1b1cbbe3645381a3a7348e8547d47a1
Rosicleia Almeida da Silva <rosialmeidadev@outlook.com>
2023-08-17 10:59:19 -0300

    services e bd

--- src/models/recesso.ts
+++ src/models/recesso.ts
@@ -3,9 +3,9 @@ import { Entity, PrimaryColumn, Column } from "typeorm"
 @Entity ("recesso")
 export default class Recesso {
 @PrimaryColumn()
-id_recesso:String
+id_recesso:string
 @Column({nullable:false})
-descricao_recesso: String
+descricao_recesso: string
 @Column({nullable:false})
 data_recesso: Date
 constructor(){
