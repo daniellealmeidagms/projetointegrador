95c426bdada7d104a9388df9bfe080f6de2fcfce
rgbds <rgbds293@gmail.com>
2023-08-18 11:37:35 -0300

    controllerscurso

--- src/controllers/controllersCurso.ts
+++ src/controllers/controllersCurso.ts
@@ -1,9 +1,52 @@
-import { Request, Response } from 'express';
+import { Request, Response, response } from 'express'
+import { servicesCursos } from '../models/servicesCursos'
+
+const service = new servicesCursos()
 
 export class controllerCurso {
-  async create(request: Request, response: Response) {}
-  async readAll(request: Request, response: Response) {}
-  async readOne(request: Request, response: Response) {}
-  async update(request: Request, response: Response) {}
-  async delete(request: Request, response: Response) {}
-}
+  async create(request: Request, response: Response){
+    const { descricao_curso, carga_horaria_curso, modalidade, eixo } =
+      request.body
+    const result = await service.create(descricao_curso,carga_horaria_curso,modalidade,eixo )
+    if (result instanceof Error) {
+      return response.status(409).json(result.message)
+    }
+    return response.status(200).json(result)
+  }
+
+  async readAll(request: Request, response: Response){
+    const result = await service.ReadAll()
+    if (result.length < 1) {
+      return response.status(204).json("Nenhum curso cadastrado!")
+    }
+    return response.status(200).json(result)
+  }
+  async readOne(request: Request, response: Response){
+    const { id_curso } = request.params
+    const result = await service.readOne({ id_curso })
+    if (result instanceof Error) {
+      return response.status(404).json(result.message)
+    }
+    return response.status(200).json(result)
+  }
+
+  async update(request: Request, response: Response){
+    const { id_curso } = request.params
+    const { descricao_curso, carga_horaria_curso, modalidade, eixo } =
+      request.body
+    const result = await service.update(id_curso, descricao_curso,carga_horaria_curso,modalidade,eixo)
+    if (result instanceof Error) {
+      return response.status(404).json(result.message)
+    }
+    return response.status(200).json(result)
+  }
+
+  async delete(request: Request, response: Response){
+    const { id_curso } = request.params
+    const result = await service.delete({ id_curso })
+    if (result instanceof Error) {
+      return response.status(404).json(result.message)
+    }
+    return response.status(300).json(result)
+  }
+} 
--- src/models/servicesCursos.ts
+++ src/models/servicesCursos.ts
@@ -23,23 +23,21 @@ export class servicesCursos {
     const cursos = await cursor.find()
     return cursos
   }
-  async readonly(id_curso) {
+  async readOne(id_curso): Promise<Curso | Error> {
     const curso = await cursor.findOne({ where: { id_curso } })
     if (!curso) {
       return new Error("Curso não existente!")
     }
     return curso
   }
-  async update(
-    id_curso,
-    carga_horaria_curso,
-    modalidade,
-    eixo
-  ): Promise<Curso | Error> {
+  async update(id_curso, descricao_curso, carga_horaria_curso, modalidade ,eixo): Promise<Curso | Error> {
     const curso = await cursor.findOne({ where: { id_curso } })
     if (!curso) {
       return new Error("Curso não existente!")
     }
+    curso.descricao_curso = descricao_curso
+      ? descricao_curso
+      : curso.descricao_curso
     curso.carga_horaria_curso = carga_horaria_curso
       ? carga_horaria_curso
       : curso.carga_horaria_curso
