e01c0700f4a292f993accc23c2e1c4b02119fc26
luisgmfdev <luisbielzinho10012002@hotmail.com>
2023-08-18 11:40:12 -0300

    Controlers Aula

--- src/services/serviceAula.ts
+++ src/services/serviceAula.ts
@@ -2,7 +2,7 @@ import { AppDataSource } from "../databases/datasource"
 import Aula from "../models/aula"
 const cursor = AppDataSource.getRepository(Aula)
 export class Serviceaula {
-  async create(data_aula, status_aula,fk_turma,fk_unidade): Promise<Aula | Error> {
+  async create(data_aula, status_aula, fk_turma, fk_unidade): Promise<Aula | Error> {
     if (await cursor.findOne({ where: { data_aula } })) {
       return new Error("Aula já cadastrada!")
     }
@@ -11,7 +11,7 @@ export class Serviceaula {
       data_aula,
       status_aula,
       fk_turma,
-      fk_unidade
+      fk_unidade,
     })
     await cursor.save(aula)
     return aula
@@ -21,22 +21,22 @@ export class Serviceaula {
     const aulas = await cursor.find()
     return aulas
   }
-  async readOne(id_aulas) {
+  async readOne(id_aulas): Promise<Aula | Error> {
     const aula = await cursor.findOne({ where: { id_aulas } })
     if (!aula) {
       return new Error("Aula não encontrada!")
     }
     return aula
   }
-  async update(id_aulas, data_aula, status_aula,fk_turma,fk_unidade): Promise<Aula | Error> {
+  async update(id_aulas, data_aula, status_aula, fk_turma, fk_unidade): Promise<Aula | Error> {
     const aula = await cursor.findOne({ where: { id_aulas } })
     if (!aula) {
       return new Error("Aula não encontrada!")
     }
     aula.data_aula = data_aula ? data_aula : aula.data_aula
     aula.status_aula = status_aula ? status_aula : aula.data_aula
-    aula.fk_turma= fk_turma? fk_turma:aula.fk_turma
-    aula.fk_unidade=fk_unidade? fk_unidade:fk_unidade
+    aula.fk_turma = fk_turma ? fk_turma : aula.fk_turma
+    aula.fk_unidade = fk_unidade ? fk_unidade : fk_unidade
     await cursor.save(aula)
     return aula
   }
