04fcf0baf9c790bb13cd48a60bc8b0e432a041aa
brunaalves <bruhousalves@gmail.com>
2023-08-21 11:55:28 -0300

    rotas

--- src/databases/datasource.ts
+++ src/databases/datasource.ts
@@ -3,7 +3,7 @@ import {DataSource} from "typeorm"
 export const AppDataSource = new DataSource ({
 type: "postgres",
 host: "localhost",
-port:5412,
+port:5432,
 username: "postgres",
 password: "postgres",
 database: "cronogramas",
---
+++ src/routes/routes.ts
@@ -0,0 +1,17 @@
+import { Router } from "express"
+import { ControllerTurma } from '../controllers/controllerTurma'
+
+const rotas = Router ()
+
+rotas.get("/", (request, response) => {
+  return response.json("home page")
+})
+
+//Turma
+rotas.post("/turma", new ControllerTurma().readAll)
+rotas.post("/turma", new ControllerTurma().create)
+rotas.get("/turma/:id_turma", new ControllerTurma().readOne)
+rotas.put("/turma/:id_turma", new ControllerTurma().update)
+rotas.delete("/turma/:id_turma", new ControllerTurma().delete)
+
+export default rotas
