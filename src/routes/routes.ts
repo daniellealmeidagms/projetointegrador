c57480d2a90433cdd47c7468fcd30c97d9ab2fad
joaopedromarque <j.p.marques.a@gmail.com>
2023-08-22 08:54:36 -0300

    server ta on [Unidade]

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -5,11 +5,11 @@ const rotas = Router()
 rotas.get("/",(request, response)=> {
   return response.json ("home page")
 })
-rotas.get("/unidade", new ControllerUnidade().readAll)
-rotas.post("/unidade", new ControllerUnidade().create)
-rotas.get("/unidade/:id_unidade", new ControllerUnidade().readOne)
-rotas.put("/unidade/:id_unidade", new ControllerUnidade().update)
-rotas.delete("/unidade/:id_unidade", new ControllerUnidade().delete)
+rotas.get("/unidades", new ControllerUnidade().readAll)
+rotas.post("/unidades", new ControllerUnidade().create)
+rotas.get("/unidade/:id_unidades", new ControllerUnidade().readOne)
+rotas.put("/unidade/:id_unidades", new ControllerUnidade().update)
+rotas.delete("/unidade/:id_unidades", new ControllerUnidade().delete)
 
 
 export default rotas
