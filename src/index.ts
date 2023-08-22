c99b973108f6a5662677c56141692bcbd2b92527
luisgmfdev <luisbielzinho10012002@hotmail.com>
2023-08-22 11:07:30 -0300

    corrigindo "a"

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { ControlerAula } from './controllers/controllerAula'
+import { Controleraula } from './controllers/controlleraula'
 import { AppDataSource } from "./databases/datasource"
 AppDataSource.initialize().then()
 import rotas from "./routes/routes"
@@ -9,8 +9,8 @@ const app = express()
 app.use(express.json())
 app.use(rotas)
 app.listen(3333, () => console.log("O server esta ONDELINE  na porta 3333. "))
-//Aula
-rotas.get("/Aula", new ControlerAula().readAll)
-rotas.post("/Aula", new ControlerAula().create)
-rotas.get("/Aula/:id_aulas",new ControlerAula().update)
-rotas.delete("/Aula/:id_aula",new ControlerAula().delete)
+//aula
+rotas.get("/aula", new Controleraula().readAll)
+rotas.post("/aula", new Controleraula().create)
+rotas.get("/aula/:id_aulas",new Controleraula().update)
+rotas.delete("/aula/:id_aula",new Controleraula().delete)
