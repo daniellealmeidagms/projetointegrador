435a2f83438884a93c38f35693e17b0c8091b490
luisgmfdev <luisbielzinho10012002@hotmail.com>
2023-08-22 11:01:18 -0300

    com "a" minusculo

--- src/models/aula.ts
+++ src/models/aula.ts
@@ -1,7 +1,7 @@
 import { Entity, PrimaryColumn, Column } from "typeorm"
 import { v4 as  uuid } from "uuid"
 @Entity("aula")
-export default class Aula {
+export default class aula {
   @PrimaryColumn()
   id_aulas: string
   @Column({ nullable: false })
