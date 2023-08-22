781ea1e10199ec90502411c0c210807828bd880c
luisgmfdev <luisbielzinho10012002@hotmail.com>
2023-08-22 11:08:25 -0300

    correcao

--- src/services/serviceAula.ts
+++ src/services/serviceAula.ts
@@ -1,10 +1,10 @@
 import { AppDataSource } from "../databases/datasource"
-import Aula from "../models/aula"
-const cursor = AppDataSource.getRepository(Aula)
+import aula from "../models/aula"
+const cursor = AppDataSource.getRepository(aula)
 export class Serviceaula {
-  async create(data_aula, status_aula, fk_turma, fk_unidade): Promise<Aula | Error> {
+  async create(data_aula, status_aula, fk_turma, fk_unidade): Promise<aula | Error> {
     if (await cursor.findOne({ where: { data_aula } })) {
-      return new Error("Aula já cadastrada!")
+      return new Error("aula já cadastrada!")
     }
 
     const aula = cursor.create({
@@ -21,17 +21,17 @@ export class Serviceaula {
     const aulas = await cursor.find()
     return aulas
   }
-  async readOne(id_aulas): Promise<Aula | Error> {
+  async readOne(id_aulas): Promise<aula | Error> {
     const aula = await cursor.findOne({ where: { id_aulas } })
     if (!aula) {
-      return new Error("Aula não encontrada!")
+      return new Error("aula não encontrada!")
     }
     return aula
   }
-  async update(id_aulas, data_aula, status_aula, fk_turma, fk_unidade): Promise<Aula | Error> {
+  async update(id_aulas, data_aula, status_aula, fk_turma, fk_unidade): Promise<aula | Error> {
     const aula = await cursor.findOne({ where: { id_aulas } })
     if (!aula) {
-      return new Error("Aula não encontrada!")
+      return new Error("aula não encontrada!")
     }
     aula.data_aula = data_aula ? data_aula : aula.data_aula
     aula.status_aula = status_aula ? status_aula : aula.data_aula
@@ -43,9 +43,9 @@ export class Serviceaula {
   async delete(id_aulas) {
     const aula = await cursor.findOne({ where: { id_aulas } })
     if (!aula) {
-      return new Error("Aula não encontrada! ")
+      return new Error("aula não encontrada! ")
     }
     await cursor.delete(aula.id_aulas)
-    return "Aula excuída com sucesso!"
+    return "aula excuída com sucesso!"
   }
 }
