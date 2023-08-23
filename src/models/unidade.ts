804cd49ba09d3777e5d284b1bbc147c00e698c92
brunaalves <bruhousalves@gmail.com>
2023-08-23 11:51:44 -0300

    filtros turma

--- src/models/unidade.ts
+++ src/models/unidade.ts
@@ -3,15 +3,17 @@ import { v4 as uuid } from "uuid"
 @Entity("unidade")
 export default class Unidade {
   @PrimaryColumn()
-  id_unidade: string
+    id_unidade: string
   @Column({ nullable: false })
-  descricao_unidade: string
+    descricao_unidade: string
   @Column({ nullable: false })
-  carga_horaria_unidade: number
+    carga_horaria_unidade: number
   @Column({ nullable: false })
-  ordem: number
+    ordem: number
+  @Column({ nullable: false })
+    turno: string
   @Column()
-  fk_curso: string
+    fk_curso: string
   constructor() {
     this.id_unidade = uuid()
   }
