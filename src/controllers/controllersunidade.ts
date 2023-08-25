2cd6a3563b56d410962f699127343133a192c93e
Patrick Pablo <138041918+pilgrimpablo@users.noreply.github.com>
2023-08-25 11:02:43 -0300

    boa pa noix familia

--- src/controllers/controllersunidade.ts
+++ src/controllers/controllersunidade.ts
@@ -1,32 +1,32 @@
 import { Request, Response } from "express"
 import { ServiceUnidade } from "../services/serviceUnidade"
-
 const service = new ServiceUnidade()
-
 export class ControllerUnidade {
   async create(request: Request, response: Response) {
     const { descricao_unidade, carga_horaria_unidade, ordem, fk_curso } = request.body
     const result = await service.create(descricao_unidade, carga_horaria_unidade, ordem, fk_curso)
     if (result instanceof Error) {
-      return response.status(400).json(result.message)
+      return response.status(409).json(result.message)
     }
-    return response.status(300).json(result)
+    return response.status(200).json(result)
   }
   async readAll(request: Request, response: Response) {
     const result = await service.readAll()
     if (result.length < 1) {
-      return response.status(204).json("Nenhuma unidade cadastrada!")
+      return response.json("Nenhuma  unidade cadastrada,")
     }
-    return response.status(200).json(result)
+    return response.status(300).json(result)
   }
+
   async readOne(request: Request, response: Response) {
     const { id_unidade } = request.params
-    const result = await service.readOne({ id_unidade })
+    const result = await service.readOne(id_unidade)
     if (result instanceof Error) {
       return response.status(404).json(result.message)
     }
-    return response.status(200).json(result)
+    return response.json(result)
   }
+
   async update(request: Request, response: Response) {
     const { id_unidade } = request.params
     const { descricao_unidade, carga_horaria_unidade, ordem, fk_curso } = request.body
@@ -40,24 +40,24 @@ export class ControllerUnidade {
     if (result instanceof Error) {
       return response.status(404).json(result.message)
     }
-    return response.status(200).json(result)
+    return response.status(300).json(result)
   }
   async delete(request: Request, response: Response) {
     const { id_unidade } = request.params
     const result = await service.delete(id_unidade)
     if (result instanceof Error) {
-      return response.status(404).json(result.message)
+      return response.status(400).json(result.message)
     }
     return response.status(300).json(result)
   }
-
   async filterTime(request: Request, response: Response) {
-    const { carga_horaria_unidade } = request.params
-    const result = await service.filterTime(carga_horaria_unidade)
-    if (result.length < 1) {
-      return response.status(204).json("Nenhuma carga horária encontrada!")
-    }
-    return response.status(200).json(result)
+  const { carga_horaria_unidade } = request.params
+  const result = await service.filterTime(carga_horaria_unidade)
+  
+  if (result.length < 1) {
+    return response.json("Nenhuma  unidade cadastrada,")
+  }
+    return response.status(300).json(result)
   }
   async filterCurso(request: Request, response: Response) {
     const { fk_curso } = request.params
