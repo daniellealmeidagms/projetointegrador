c1eb49fedf553af3f7168af2e2dc4cf0f5fd4bbd
luisgmfdev <luisbielzinho10012002@hotmail.com>
2023-08-25 10:49:17 -0300

    final

--- src/controllers/controllerAula.ts
+++ src/controllers/controllerAula.ts
@@ -1,17 +1,23 @@
 import { Request, Response } from "express"
-import { Serviceaula } from "../services/serviceAula"
+import { ServiceAula } from "../services/serviceAula"
 
-const service = new Serviceaula()
+const service = new ServiceAula()
 
-export class ControlerAula {
+export class ControllerAula {
   async create(request: Request, response: Response) {
     const { data_aula, status_aula, fk_turma, fk_unidade } = request.body
-    const result = await service.create(data_aula, status_aula, fk_turma, fk_unidade)
+    const result = await service.create(
+      data_aula,
+      status_aula,
+      fk_turma,
+      fk_unidade
+    )
     if (result instanceof Error) {
       return response.status(409).json(result.message)
     }
-    return response.status(300).json(result)
+    return response.status(200).json(result)
   }
+
   async readAll(request: Request, response: Response) {
     const result = await service.readAll()
     if (result.length < 1) {
@@ -19,58 +25,65 @@ export class ControlerAula {
     }
     return response.status(200).json(result)
   }
+
   async readOne(request: Request, response: Response) {
     const { id_aula } = request.params
-    const result = await service.readOne({ id_aula })
+    const result = await service.readOne(id_aula)
     if (result instanceof Error) {
       return response.status(404).json(result.message)
     }
     return response.status(200).json(result)
   }
+
   async update(request: Request, response: Response) {
     const { id_aula } = request.params
     const { data_aula, status_aula, fk_turma, fk_unidade } = request.body
-    const result = await service.update(id_aula, data_aula, status_aula, fk_turma, fk_unidade)
+    const result = await service.update(
+      id_aula,
+      data_aula,
+      status_aula,
+      fk_turma,
+      fk_unidade
+    )
     if (result instanceof Error) {
       return response.status(404).json(result.message)
     }
-    return response.json(result)
+    return response.status(200).json(result)
   }
+
   async delete(request: Request, response: Response) {
     const { id_aula } = request.params
-    const result = await service.delete({ id_aula })
+    const result = await service.delete(id_aula)
     if (result instanceof Error) {
       return response.status(400).json(result.message)
     }
     return response.status(300).json(result)
   }
+
   async filter_data_aula(request: Request, response: Response) {
     const { data_aula } = request.params
-    const result = await service.filter_data_aula({ data_aula })
-
+    const result = await service.filter_data_aula(data_aula)
     if (result instanceof Error) {
-      return response.status(400).json(result)
+      return response.status(400).json(result.message)
     }
+    return response.status(300).json(result)
+  }
 
-    return response.status(200).json(result)
+  async filter_turma(request: Request, response: Response) {
+    const { fk_turma } = request.params
+    const result = await service.filter_turma(fk_turma)
+    if (result instanceof Error) {
+      return response.status(400).json(result.message)
+    }
+    return response.status(300).json(result)
   }
-async filter_turma(request: Request, response: Response) {
-  const { fk_turma } = request.params;
-  const result = await service.filter_turma({ fk_turma })
-  
-  if (result instanceof Error) {
-    return response.status(400).json(result.message);
-  }
-  
-  return response.status(200).json(result);
-}
-async filter_status(request: Request, response: Response){
-  const {status_aula} = request.params
-  const result = await service.filter_status({ status_aula })
-  if(result instanceof Error){
-    return response.status(400).json(result.message)
-  }
-  return response.status(200).json(result)
-}
-}
 
+  async filter_status(request: Request, response: Response) {
+    const { status_aula } = request.params
+    const result = await service.filter_status(status_aula)
+    if (result instanceof Error) {
+      return response.status(400).json(result.message)
+    }
+    return response.status(300).json(result)
+  }
+}
